782b3d5b5c03b85c30ad05966f3bb358f2daa0c4
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-07-29 11:29:54 -0500

    added integration tests and moved serving function to server.js

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-var http = require('http');
-var https = require('https');
-var cp = require('child_process');
+require('./lib/server')(1337);
 
-
-http.createServer(function (req, res) {
-
-  var args = req.url.split('/');
-
-  if(req.method==='GET' && req.url==='/starwars'){
-    res.writeHeader(200, {
-      'Content-Type' : 'application/json',
-      'Access-Control-Allow-Origin' : '*'
-    });
-    https.get('https://swapi.co/api/films/')
-      .on('response', function (xhr) {
-        xhr.pipe(res);
-      });
-    }else if(args[1]==='cal'){
-      res.writeHeader(200, {
-        'Content-Type' : 'application/json',
-        'Access-Control-Allow-Origin' : '*'
-      });
-      var arguments = args.splice(2);
-      console.log(arguments);
-      var output = cp.execFileSync('/Users/Greg/Documents/NSS/code/calendar-cli/cal.js', arguments);
-      res.write(output);
-      res.end();
-    }else{
-      res.writeHead(403);
-      res.end('Access Denied');
-    }
-
-}).listen(1337);
-
-console.log('Server running on http://localhost:1337')
+console.log('Server running on http://localhost:1337');
---
+++ test/integration/routes.test.js
@@ -0,0 +1,69 @@
+var expect = require('chai').expect;
+var http = require('http');
+var path = require('path');
+
+describe('routes', function(){
+  this.timeout(30000);
+  var port = Math.floor(Math.random()*50000+10000);
+  //start server before test
+  before(function(){
+    require(path.join(process.cwd(), '/lib/server'))(port);
+  });
+
+  //test the cal route
+  it('should respond to the /cal route', function(done){
+    http.get('http://localhost:' + port + '/cal', function(res){
+      var body = '';
+
+      expect(res.statusCode).to.equal(200);
+
+      res
+      .on('data', function(chunk){
+        body += chunk;
+      })
+      .on('end', function(){
+        expect(body).to.contain('1');
+        done();
+      })
+    })
+  });
+
+  //test the cal route with params
+  it('should respond to the /cal/7/1999 route', function(done){
+    http.get('http://localhost:' + port + '/cal/7/1999', function(res){
+      var body = '';
+
+      expect(res.statusCode).to.equal(200);
+
+      res
+      .on('data', function(chunk){
+        body += chunk;
+      })
+      .on('end', function(){
+        expect(body).to.contain('1999');
+        done();
+      })
+    })
+  });
+
+  //test the starwars route
+  it('should respond to the /starwars route', function(done){
+    http.get('http://localhost:' + port + '/starwars', function(res){
+      var body = '';
+
+      expect(res.statusCode).to.equal(200);
+
+      res
+      .on('data', function(chunk){
+        body += chunk;
+      })
+      .on('end', function(){
+        expect(body).to.contain('A New Hope');
+        done();
+      })
+    })
+  });
+
+  //display the port number
+  console.log('Server is running on localhost:', port);
+});
