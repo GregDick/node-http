41d815211638ffe1f1e114ac2e8fdffafc9df59b
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-07-30 15:27:59 -0500

    changed port variable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var chalk = require('chalk');
+var port = process.env.PORT || 1337;
+require('./lib/server')(port);
 
-require('./lib/server')(1337);
 
-
-console.log('Server running on ' + chalk.bgMagenta('http://localhost:1337'));
+console.log('Server running on ' + chalk.bgMagenta('http://localhost:' + port));
